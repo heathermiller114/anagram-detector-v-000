651b32f8a7f39d29fb49cca27b3d650f3659fd61
Heather Miller <heather.miller114@gmail.com>
2018-09-18 22:11:12 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,4 +1,5 @@
 # Your code goes here!
+require 'pry'
 class Anagram
 
   def initialize(word)
@@ -6,6 +7,8 @@ class Anagram
   end
   
   def match(word)
+    word.split("").sort
+    binding.pry
   end
 
 end
