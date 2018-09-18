4d0714eb6fcc1233d6c5f97d34d9312cd5799e09
Heather Miller <heather.miller114@gmail.com>
2018-09-18 23:22:23 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,10 +7,7 @@ class Anagram
   end
   
   def match(word_array)
-    single_word_array"#{counter+1}" = []
-    counter = 0
-    word_array.each do |word|
-      single_word_array"#{counter}+1"<< word_array[counter]
+
     binding.pry
   end
 
